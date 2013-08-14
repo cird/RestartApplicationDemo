b69e452198cc12a41c1b162ef614b80da3012f12
HernanEche <hernan.eche@gmail.com>
2013-08-14 16:27:59 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Demo from
+
+[Java application self-restart](http://cplusadd.blogspot.com.ar/2009/04/java-application-and-self-restart.html)
---
+++ src/restartapplicationdemo/RestartApplicationDemo.java
@@ -0,0 +1,67 @@
+package restartapplicationdemo;
+
+import java.io.File;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ *
+ * @author Carlos Becker
+ * from http://cplusadd.blogspot.com.ar/2009/04/java-application-and-self-restart.html
+ * 
+ * (this demo by Hernan Echegoyemberry)
+ */
+public class RestartApplicationDemo {
+
+    RestartApplicationDemo(){
+        
+        int option=JOptionPane.showConfirmDialog( null, "This program does nothing (until you press cancel.)",
+                                       "Doing nothing", JOptionPane.OK_CANCEL_OPTION);
+        
+        if(option!=JOptionPane.OK_OPTION) throw new IllegalArgumentException();
+            
+    }        
+   
+    public static void main(String[] args) {                               
+        try{
+            
+            RestartApplicationDemo.restartApplication(new RestartApplicationDemo());
+            
+        }catch(IllegalArgumentException ex)
+        {
+            //exits (without restart)
+        }
+        
+    }
+    
+    static public boolean  restartApplication( Object classInJarFile )
+    {
+        String javaBin = System.getProperty("java.home") + "/bin/java";
+        File jarFile;
+        try{
+            jarFile = new File
+            (classInJarFile.getClass().getProtectionDomain()
+            .getCodeSource().getLocation().toURI());
+        } catch(Exception e) {        
+            return false;
+        }
+
+        /* is it a jar file? */
+        if ( !jarFile.getName().endsWith(".jar") )        
+        return false;//no, it's a .class probably
+
+        String  toExec[] = new String[] { javaBin, "-jar", jarFile.getPath() };
+        try{
+            Process p = Runtime.getRuntime().exec( toExec );
+        } catch(Exception e) {
+            e.printStackTrace(System.out);
+            return false;
+        }
+        
+        System.exit(0);
+
+        return true;
+    }
+
+
+}
